30850f59d73dea774765d0411d1a8c35a2911dde
zhangjie <768284730@qq.com>
2026-03-04 00:05:28 +0800

    更新README.md并清理测试文件

--- check-user-router.js
+++
@@ -1,6 +0,0 @@
-const userRouter = require('./src/routes/user/user');
-
-console.log('User router routes:');
-userRouter.stack.forEach((route, index) => {
-    console.log(`${index + 1}. ${route.methods.join(', ')} ${route.path}`);
-});
